d9024b1bd9180c58aee638121f3d45ae3098a238
uojin29 <uojin29@gmail.com>
2024-07-18 05:36:42 +0900

    feat: 입장코드 입력시 ta, st권한에 맞게 입장 (#139)

--- src/main/java/com/example/classhub/domain/classhub_lroom/controller/LectureRoomController.java
+++ src/main/java/com/example/classhub/domain/classhub_lroom/controller/LectureRoomController.java
@@ -14,6 +14,7 @@ import com.example.classhub.domain.tag.controller.response.TagListResponse;
 import com.example.classhub.domain.tag.service.TagService;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -61,6 +62,20 @@ public class LectureRoomController {
         if(studentFile != null && !studentFile.isEmpty())
             memberLRoomService.createMemberLRoom(lectureRoomDto.getLectureRoomId(), studentFile);
         return "redirect:/lecture-room";
+    }
+
+    @GetMapping("/lecture-room/verifyInviteCode")
+    public ResponseEntity<Boolean> verifyInviteCode(@RequestParam String inviteCode, @RequestParam Long lectureRoomId, HttpSession session) {
+        System.out.println("Controller invite code = " + inviteCode);
+        MemberDto memberDto = (MemberDto) session.getAttribute("member");
+        LectureRoomDto lectureRoomDto = lectureRoomService.findLRoomDtoByRoomId(lectureRoomId);
+        try {
+            boolean isValid = lectureRoomService.verifyInviteCode(lectureRoomId, inviteCode);
+            memberLRoomService.createMemberByOne(lectureRoomDto, memberDto, inviteCode);
+            return ResponseEntity.ok(isValid);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(false);
+        }
     }
 
     @GetMapping("/lecture-room/detail/{lectureRoomId}")
